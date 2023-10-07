18776ea486773d6475a97cdc69832faea4081473
Hashim-Khan0 <139082859+Hashim-Khan0@users.noreply.github.com>
2023-10-07 15:54:10 -0400

    barebones code for the backend language model

--- GoldenHack backend.py
+++ GoldenHack backend.py
@@ -1,6 +1,20 @@
 import assemblyai as aai
+from secret_case import API_TOKEN
 
-transcriber = aai.Transcriber()
-transcript = transcriber.transcribe("https://storage.googleapis.com/aai-web-samples/gettysburg.wav")
+transcriber = aai.Transcriber(API_TOKEN)
+transcript = transcriber.transcribe("https://github.com/AssemblyAI-Examples/audio-examples/raw/main/20230607_me_canadian_wildfires.mp3")
 
 print(transcript.text)
+
+params = {
+    "context": "There was a fire",
+    "answer_format": "Bullet points"
+}
+
+result = transcript.lemur.summarize(**params)
+
+print(result.response)
+
+to_translate = result.response
+translated = GoogleTranslator(source='auto', target='fr').translate(to_translate)
+print(translated)
