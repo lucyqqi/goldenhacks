2d1b9869ea2c7582063ab5b2d9b2cac40adfdf98
Hashim-Khan0 <139082859+Hashim-Khan0@users.noreply.github.com>
2023-10-07 19:21:45 -0400

    Update example.py

--- example.py
+++ example.py
@@ -1,3 +1,17 @@
 from taipy import Gui
 
-Gui(page="# Getting started with *Taipy*").run(use_reloader=True)
+# Add a navbar to switch from one page to the other
+root_md = """
+<|navbar|>
+# eduscribe.ai
+"""
+page1_md = "## Upload your file"
+page2_md = "## transcript and/or translation"
+
+pages = {
+    "/": root_md,
+    "page1": page1_md,
+    "page2": page2_md
+}
+Gui(pages=pages).run(use_reloader=True)
+
