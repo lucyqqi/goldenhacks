aa865f363673efb630da370325e65d19f3a918e1
Hashim-Khan0 <139082859+Hashim-Khan0@users.noreply.github.com>
2023-10-07 23:47:06 -0400

    hello

--- backend.py
+++ backend.py
@@ -2,43 +2,26 @@ import assemblyai as aai
 from secret_case import API_TOKEN
 from deep_translator import GoogleTranslator
 
-aai.settings.api_key = API_TOKEN
+def translate_audio_to_language():
+    # Initialize the AssemblyAI API
+    aai.settings.api_key = API_TOKEN
 
-FILE_URL = "https://github.com/AssemblyAI-Examples/audio-examples/raw/main/20230607_me_canadian_wildfires.mp3"
+    FILE_URL = "https://github.com/AssemblyAI-Examples/audio-examples/raw/main/20230607_me_canadian_wildfires.mp3"
 
-transcriber = aai.Transcriber()
-transcript = transcriber.transcribe(FILE_URL)
+    transcriber = aai.Transcriber()
+    transcript = transcriber.transcribe(FILE_URL)
 
-# IF X IS TRUE: summarizer
+    # IF X IS TRUE: summarizer
 
-params = {
-    "context": "There was a fire",
-    "answer_format": "Bullet points"
-}
+    params = {
+        "context": "There was a fire",
+        "answer_format": "Bullet points"
+    }
 
-result = transcript.lemur.summarize(**params)
+    result = transcript.lemur.summarize(**params)
 
-to_translate = result.response
+    to_translate = result.response
 
-translated = GoogleTranslator(source='auto', target='zh-CN').translate(to_translate)
+    translated = GoogleTranslator(source='auto', target='zh-CN').translate(to_translate)
 
-
-# Read
-"""
-with open("output.html", "r", encoding="utf-8") as file:
-    html_content = file.read()
-"""
-
-
-# Replace 
-"""
-html_content = html_content.replace("%TRANSLATED_TEXT%", translated)
-"""
-
-
-# Write
-"""
-with open("output.html", "w", encoding="utf-8") as file:
-    file.write(html_content)
-"""
-
+    return translated
