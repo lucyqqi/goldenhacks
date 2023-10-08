18b99ee67d7bdd41e42a14c9fc363817ebbe7c14
Hashim-Khan0 <139082859+Hashim-Khan0@users.noreply.github.com>
2023-10-08 04:40:25 -0400

    final backend

--- frontend_stuff/backend.py
+++ frontend_stuff/backend.py
@@ -7,32 +7,24 @@ def translate_audio_to_language():
     aai.settings.api_key = API_TOKEN
 
     FILE_URL = "https://github.com/AssemblyAI-Examples/audio-examples/raw/main/20230607_me_canadian_wildfires.mp3"
-    FILE_URL2 = "https://github.com/Hashim-Khan0/mp3/blob/main/Emmanuel%20Macron's%20inauguration%20speech_%20France%20will%20always%20work%20on%20building%20long-term%20peace.mp3"
-
+    
     transcriber = aai.Transcriber()
-    transcriptC = transcriber.transcribe(FILE_URL)
-    transcriptF = transcriber.transcribe(FILE_URL2)
+    transcript = transcriber.transcribe(FILE_URL)
 
 
     # IF X IS TRUE: summarizer
 
-    paramsC = {
+    params = {
         "context": "There was a fire",
         "answer_format": "Bullet points"
     }
 
-    paramsF = {
-        "context": "President of France innaguration speech",
-        "answer_format": "summarry"
-    }
 
-    resultC = transcriptC.lemur.summarize(**paramsC)
-    resultF = transcriptF.lemur.summarize(**paramsF)
+    result = transcript.lemur.summarize(**params)
 
-    to_translate = resultC.response
-    to_translate2 = resultF.response
+    to_translate = result.response
 
-    translatedC = GoogleTranslator(source='auto', target='zh-CN').translate(to_translate)
-    translatedF = GoogleTranslator(source='auto',target='en').translate(to_translate2)
+    translated = GoogleTranslator(source='auto', target='zh-CN').translate(to_translate)
 
-    return translatedC, translatedF
+
+    return translated
